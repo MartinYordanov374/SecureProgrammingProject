ab32a13f4fd5710a77d4f608eb45e2a5be03c9f2
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-20 22:32:47 +0200

    Add register form

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import NavigationBar from './Navbar.jsx'
-
+import {Form, FormControl, Button, Container} from 'react-bootstrap'
+import '../Styles/Login/LoginStyles.css'
 export default function Register() {
   return (
     <div>
        <NavigationBar/>
-       Register
+       <Container className='LoginContainer'>
+        <p>Sign up</p>
+        <Form>
+          <FormControl 
+          className='FormField'
+          placeholder='Enter username'
+          />
+          <FormControl 
+          className='FormField'
+          placeholder='Enter password'
+          />
+           <FormControl 
+          className='FormField'
+          placeholder='Confirm password'
+          />
+        </Form>
+        <Button className='LoginBtn'>Sign up</Button>
+      </Container>
     </div>
   )
 }
