c70c2c5d1bde140d705ab18bb93369dc9e49a263
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-31 21:07:52 +0300

    Add login and register endpoints for the server

--- src/Backend/Express/server.js
+++ src/Backend/Express/server.js
@@ -3,8 +3,16 @@ const connectToMongoDb = require('../Mongo/Mongoose/mongoose')
 
 const app = express()
 const APP_PORT = 5001
+
 app.use(express.json())
 
+app.post('/login', async (req,res)  => {
+    console.log('logging in')
+})
+
+app.post('/register', async (req,res) => {
+    console.log('registering')
+})
 
 app.listen(APP_PORT, async (res) => {
     await connectToMongoDb()
