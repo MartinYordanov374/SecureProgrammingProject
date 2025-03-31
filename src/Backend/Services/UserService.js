5055a3d9ccb9583b5ecc15c4de3a6c96e170ca55
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-31 21:29:32 +0300

    Add user services backbone

---
+++ src/Backend/Services/UserService.js
@@ -0,0 +1,14 @@
+async function CreateUser(){
+    console.log('creating user')
+}
+
+async function DeleteUser(){
+    console.log('deleting user')
+}
+
+async function IfUserExists(){
+    console.log('checking if user exists')
+}
+
+
+module.exports = {CreateUser, DeleteUser, IfUserExists}
