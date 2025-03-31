0a4bfe24c92901a40d5f5b8bb202ffe96f5c3af8
MartinYordanov374 <martinyordanov284@gmail.com>
2025-03-31 22:13:00 +0300

    Fix critical mongodb issues related to db connection and schema creation

--- src/Backend/Express/server.js
+++ src/Backend/Express/server.js
@@ -4,6 +4,8 @@ const connectToMongoDb = require('../Mongo/Mongoose/mongoose')
 const app = express()
 const APP_PORT = 5001
 
+const {CreateUser, IfUserExists} = require('../Services/UserService.js')
+
 app.use(express.json())
 
 app.post('/login', async (req,res)  => {
@@ -11,7 +13,17 @@ app.post('/login', async (req,res)  => {
 })
 
 app.post('/register', async (req,res) => {
-    console.log('registering')
+    let username = req.body.username
+    let password = req.body.password
+
+    await CreateUser(username, password).then((res) => {
+        console.log(res)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+    res.status(200).send({username, password})
 })
 
 app.listen(APP_PORT, async (res) => {
