9f26664a22846bd3d0c78112e67caf6d3158d152
MartinYordanov374 <martinyordanov284@gmail.com>
2025-04-04 16:40:23 +0300

    Add conditional navbar rendering

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,55 @@
 import React from 'react'
 import {NavItem, Navbar, NavLink, NavbarBrand, Nav} from 'react-bootstrap'
-export default function NavigationBar() {
+import useAuth from '../Hooks/useAuth.js'
+export default function NavigationBar() 
+{
+  const [isRegistered, isLoading] = useAuth();
+
   const handleLogOut = () => {
     console.log('logging out')
   }
-  return (
-    <Navbar>
-      <NavbarBrand href='/'> SEPRO </NavbarBrand>
-      <Nav>
-        <NavItem>
-            <NavLink href='/'> Home </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href='/login'> Log in </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href='/register'> Sign up </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink href='/Profile/:id'> My profile</NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink onClick={() => {handleLogOut()}}> Log out</NavLink>
-        </NavItem>
-      </Nav>
-    </Navbar>
-  )
+  if(isRegistered)
+  {
+    return (
+      <Navbar>
+        <NavbarBrand href='/'> SEPRO </NavbarBrand>
+        <Nav>
+          <NavItem>
+              <NavLink href='/'> Home </NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink href='/login'> Log in </NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink href='/register'> Sign up </NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink href='/Profile/:id'> My profile</NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink onClick={() => {handleLogOut()}}> Log out</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
+  else
+  {
+    return (
+      <Navbar>
+        <NavbarBrand href='/'> SEPRO </NavbarBrand>
+        <Nav>
+          <NavItem>
+              <NavLink href='/'> Home </NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink href='/login'> Log in </NavLink>
+          </NavItem>
+          <NavItem>
+              <NavLink href='/register'> Sign up </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
 }
