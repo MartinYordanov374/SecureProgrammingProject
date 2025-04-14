4c281f10954510159294f4c12215167ce92953e7
MartinYordanov374 <martinyordanov284@gmail.com>
2025-04-14 13:29:35 +0300

    Add create post modal

--- src/Components/CreatePostField/CreatePost.jsx
+++ src/Components/CreatePostField/CreatePost.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CreatePostStyles.css'
-import { Card } from 'react-bootstrap'
+import { Button, Card, FormCheck, FormControl, Modal } from 'react-bootstrap'
 
-export default function CreatePost() {
+export default function CreatePost() 
+{
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const hideModal = () => {
+    setIsModalOpen(false)
+  }
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+  const handleCreatePostClick = () => {
+    if(isModalOpen)
+    {
+        hideModal()
+    }
+    else
+    {
+        showModal()
+    }
+  }
   return (
+    <>
     <Card 
-    placeholder='Create a post'
     className='CreatePostField'
+    onClick={() => {handleCreatePostClick()}}
     >
       How is your cyber security journey going?
     </Card>
+
+    <Modal show={isModalOpen} onHide={hideModal} onShow={showModal}>
+        <Modal.Header>Create post</Modal.Header>
+        <Modal.Body>
+            <FormControl 
+                as='textarea'
+                rows={2}
+                placeholder='How is your cyber security journey going?'
+                className='CreatePostModalInputField'
+            />
+        </Modal.Body>
+        <Modal.Footer>
+            <Button className='btn-danger' onClick={() => hideModal()}>Cancel</Button>
+            <Button>Publish</Button>
+        </Modal.Footer>
+    </Modal>
+    </>
+    
   )
 }
