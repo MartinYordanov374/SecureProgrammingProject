712868c3e176559c41f5c3731bcc8196f88c0ca5
MartinYordanov374 <martinyordanov284@gmail.com>
2025-04-14 13:02:38 +0300

    Add create post field

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'
 import NavigationBar from '../Navbar/Navbar.jsx'
 import Post from '../Post/Post.jsx'
 import Axios from 'axios'
-
+import useAuth from '../../Hooks/useAuth.js'
+import './HomeStyles.css'
+import { Card } from 'react-bootstrap'
 export default function Home() {
   const [posts, setPosts] = useState([])
   
+  const [isRegistered, isLoading] = useAuth()
+
   const GetPosts = async() => {
     await Axios.get('http://localhost:5001/post/getAll',{withCredentials: true})
     .then((res) => {
@@ -21,6 +25,12 @@ export default function Home() {
   return (
     <div>
       Home  
+      <Card 
+      placeholder='Create a post'
+      className='CreatePostField'
+      >
+        How is your cyber security journey going?
+      </Card>
       {posts.length > 0 ?
         posts.map((post) => {
         return(<Post postObject={post}/>)
